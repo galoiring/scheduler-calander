7a83f84aa3d48adb5c898cdfbedc7162c082ea50
Gal Oiring <gal.oiring@kyndryl.com>
2023-07-27 16:50:16 +0300

    test route

--- flask-backend/app.py
+++ flask-backend/app.py
@@ -1,11 +1,19 @@
 from flask import Flask
+from flask_cors import CORS
 
+# Initialize Flask app
 app = Flask(__name__)
+cors = CORS(app)
 
 
 @app.route('/')
 def hello():
     return 'Hello from Flask backend!'
+
+
+@app.route('/list_test', methods=['GET'])
+def list_datasets():
+    return 'list of all the calanders'
 
 
 if __name__ == '__main__':
